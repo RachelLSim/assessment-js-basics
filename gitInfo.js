74d4597cc78167e3258155865ae2d92a40ab562c
Rachel Simonsen <rachellsimonsen@gmail.com>
2022-03-04 09:07:20 -0700

    intial commit

---
+++ gitInfo.js
@@ -0,0 +1,103 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+var gitDefinition = ('git is a method for tracking coding projects through various stages by creating snapshots of code and storing them in a repository.')
+console.log(gitDefinition)
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+var gitHubDefinition = ('GitHub is an online storage method where repositories can be uploaded and shared to track coding projects')
+console.log(gitHubDefinition)
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+
+var gitInitDefinition = ('git init initializes a local repository in the folder you are currently using')
+console.log(gitInitDefinition)
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+
+var gitCloneDefinition = ('git clone copies a reository from GitHub and puts it onto my computer.')
+console.log(gitCloneDefinition)
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+
+var gitStatusDefinition = ('git status tells the user what files are being tracked, what changes have been committed and what still needs to be committed to the repository. Basically gives you information about your git tracking.')
+console.log(gitStatusDefinition)
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+
+gitAddDefinition = ('git add adds files to be tracked to the repository')
+console.log(gitAddDefinition)
+
+var gitAddCode = ('git add .')
+console.log(gitAddCode)
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+var gitCommitCode = ('')
+console.log(gitCommitCode)
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+
+
+var gitPushDefinition = ('')
+console.log(gitPushDefinition)
