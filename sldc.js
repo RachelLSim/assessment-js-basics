01f69d9727a4565abf4ffaddf1768670458c9ab6
Rachel Simonsen <rachellsimonsen@gmail.com>
2022-03-04 09:45:44 -0700

    sdlc finished

--- sldc.js
+++ sldc.js
@@ -12,6 +12,8 @@
 // Planning
 // Testing & Integration
 
+console.log("Planning, Analysis of Requirements, Design, Implementation, Testing & Intergration, Maintain"
+)
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +21,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = ('This is the second step, the defining or feasibility phase. Team members decide how to proceed and analyze whether the project is feasible in allotted timeframe and budget, with available resources.')
+const design = ('Third step, the team creates a plan or blueprint for the product, deciding what the end result will look like and accomplish.')
+const implementation = ('Fourth step, the building and coding of a prototype.')
+const testingIntegration = ('Fifth step, this step tests the product and checks for bugs and errors. Bugs are reported and returned to engineers who then fix them and run testing again.')
+const maintenance = ('Last step, after product is deployed this involves solving issues with the product that may come up over time.')
